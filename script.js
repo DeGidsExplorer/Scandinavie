aca537292d90aad2d3bcd850d258f91e39868945
DeGidsExplorer <colinvanhulst@beatrixschoolnieuwegein.nl>
2026-06-16 09:24:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Menu geladen");
+document.addEventListener("DOMContentLoaded", () => {
+    const card = document.querySelector(".card");
+
+    document.addEventListener("mousemove", (e) => {
+        const x = (e.clientX / window.innerWidth - 0.5) * 10;
+        const y = (e.clientY / window.innerHeight - 0.5) * 10;
+
+        card.style.transform = `rotateY(${x}deg) rotateX(${-y}deg)`;
+    });
+});
